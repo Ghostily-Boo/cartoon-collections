7dd5be01eb17da7c68fb32d5ef6c6cd0d5e9a62f
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-10-26 17:24:59 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array)
+  array.each_with_index do |array, index|
+    index += 1
+    puts "#{index}. #{array}./n"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|array| "#{array.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  short = false
+  array.each do |array|
+    if "#{array}".length > 4
+    short = true
+    end
+  end
+  short
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
+  yum = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if snacks.include?(cheese)
+     yum = cheese
+     break
+    end
+  end
+  yum
 end
